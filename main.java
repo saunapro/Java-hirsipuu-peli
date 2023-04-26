9d33857b38d670484f8ef9e302c3f46c1741462b
Ville Ollikainen <125296864+saunapro@users.noreply.github.com>
2023-04-26 11:47:56 +0300

    Ohjelman sanalistan lukemisen toteutusratkaisu

--- main.java
+++ main.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class main {
 
-    public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);               
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        Scanner tiedostonlukija = new Scanner(new File("sanalista.txt")); //tekstitiedosto
+
+        Scanner lukija = new Scanner(System.in);
+
+        List<String> sanat = new ArrayList<>();
+
+        while (tiedostonlukija.hasNextLine()) {
+            sanat.add(tiedostonlukija.nextLine());
+        }
+
+        System.out.println(sanat);
+
       
     }
 }
