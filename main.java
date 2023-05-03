cc8f61fccd5844392d93f4a14b1564101d4b3aaa
Ville Ollikainen <ville.ollikainen2@esedulainen.fi>
2023-05-03 09:20:52 +0300

    satunnaisen sana valitseminen

--- main.java
+++ main.java
@@ -1,10 +1,29 @@
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class main {
 
-    public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);               
-      
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner tiedostonlukija = new Scanner(new File("sanalista.txt")); // tekstitiedosto
+
+        Scanner lukija = new Scanner(System.in);
+
+        List<String> sanat = new ArrayList<>();
+
+        while (tiedostonlukija.hasNextLine()) {
+            sanat.add(tiedostonlukija.nextLine());
+        }
+
+        Random random = new Random();
+
+        String sana = sanat.get(random.nextInt(sanat.size()));
+
+        System.out.println(sana);
+
     }
 }
